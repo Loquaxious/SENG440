2130eff3bcb6b5d9c9535036a83c6fd2eb7ea1d4
Logan <lpl29@uclive.ac.nz>
2022-08-19 17:57:38 +1200

    more tutorial work and adding assigmnet specs

---
+++ Tutorials/Tut5/LiveDataConnect440/app/src/main/java/com/example/a440tut3connect440/Friend.kt
@@ -0,0 +1,9 @@
+package com.example.a440tut3connect440
+
+class Friend (val name: String,
+              val slackId: String,
+              val home: String,
+              val email: String,
+              val phone: String) {
+    override fun toString() = name
+}
