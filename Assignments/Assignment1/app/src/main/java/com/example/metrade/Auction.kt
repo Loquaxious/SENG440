7d767798d09b1fdf71ab3ecf326a0ef31f9c4a0a
Logan Lee <lpl29@uclive.ac.nz>
2022-08-22 00:02:32 +1200

    Started on assignment 1

---
+++ Assignments/Assignment1/app/src/main/java/com/example/metrade/Auction.kt
@@ -0,0 +1,16 @@
+package com.example.metrade
+
+
+class Auction (
+    val auctionId: Int,
+    val title: String,
+    val description: String,
+    val endDate: String,
+    val categoryId: Int,
+    val reserve: Int,
+    val sellerId: Int,
+    val sellerFirstName: String,
+    val sellerLastName: String,
+    val numBids: Int,
+    val highestBid: Int,
+        )
