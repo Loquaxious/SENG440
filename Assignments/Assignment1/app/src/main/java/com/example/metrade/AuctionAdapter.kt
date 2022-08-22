7581ccc08be055e1bd811ca2d12dad4af566879b
Loquaxious <loganpono@gmail.com>
2022-08-23 11:33:47 +1200

    Adapter for recycler view and tut6

---
+++ Assignments/Assignment1/app/src/main/java/com/example/metrade/AuctionAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.metrade
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class AuctionAdapter(private var auctions: List<Auction>, private val onAuctionListener: OnAuctionListener)
+    : RecyclerView.Adapter<AuctionAdapter.AuctionViewHolder>() {
+
+    class AuctionViewHolder(itemView: View, val onAuctionListener: OnAuctionListener)
+        : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        val title: TextView
+        val currentBid: TextView
+        val auctionImage: ImageView
+
+        init {
+            title = itemView.findViewById(R.id.auction_title)
+            currentBid = itemView.findViewById(R.id.auction_bid)
+            auctionImage = itemView.findViewById(R.id.auction_image)
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View?) {
+            onAuctionListener.onAuctionClick(adapterPosition)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AuctionViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_auction, parent, false)
+        return AuctionViewHolder(view, onAuctionListener)
+    }
+
+    override fun onBindViewHolder(viewHolder: AuctionViewHolder, position: Int) {
+        viewHolder.title.text = auctions[position].title
+        viewHolder.currentBid.text = "$${auctions[position].highestBid}"
+        Picasso.get().load(BASE_URL + "auctions/${auctions[position].auctionId}/image").into(viewHolder.auctionImage)
+    }
+
+    override fun getItemCount() = auctions.size
+
+    fun setAuctions(newAuctions: List<Auction>) {
+        auctions = newAuctions
+        notifyDataSetChanged()
+    }
+
+    interface OnAuctionListener {
+        fun onAuctionClick(position: Int)
+    }
+}
