108b7c957642462439af54704a7efe4e0b402afd
Logan <lpl29@uclive.ac.nz>
2022-08-24 23:27:12 +1200

    Completed sort/filter activity

--- Assignments/Assignment1/app/src/main/java/com/example/metrade/MainActivity.kt
+++ Assignments/Assignment1/app/src/main/java/com/example/metrade/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.metrade
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +16,7 @@ import kotlinx.coroutines.launch
 import java.net.URL
 
 const val BASE_URL = "https://seng365.csse.canterbury.ac.nz/api/v1/"
+const val SORT_ACTIVITY = 1628420694
 
 class LandingActivity : AppCompatActivity(), AuctionAdapter.OnAuctionListener {
     private var auctions : List<ListAuction> = listOf()
@@ -25,13 +28,12 @@ class LandingActivity : AppCompatActivity(), AuctionAdapter.OnAuctionListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getAuctions()
+        getAuctions(URL(BASE_URL + "auctions"))
 
         sortButton = findViewById(R.id.sortButton)
         sortButton.setOnClickListener {
             val intent = Intent(this, SortFilterActivity::class.java)
-            startActivity(intent)
-
+            startActivityForResult(intent, SORT_ACTIVITY)
         }
 
         auctionAdapter = AuctionAdapter(auctions, this)
@@ -41,9 +43,9 @@ class LandingActivity : AppCompatActivity(), AuctionAdapter.OnAuctionListener {
     }
 
 
-    private fun getAuctions() {
+    private fun getAuctions(url: URL) {
         lifecycleScope.launch {
-            val result = getJson(URL(BASE_URL + "auctions"))
+            val result = getJson(url)
             val jsonAuctions = result.getJSONArray("auctions")
             auctions = (0 until jsonAuctions.length()).map { i ->
                 val auction = jsonAuctions.getJSONObject(i)
@@ -64,6 +66,33 @@ class LandingActivity : AppCompatActivity(), AuctionAdapter.OnAuctionListener {
         startActivity(intent)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SORT_ACTIVITY && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                filterAuctions(data.getStringExtra("query").toString(),
+                    data.getIntExtra("category", 0),
+                    data.getIntExtra("sort", 0))
+            }
+            Toast.makeText(this, getString(R.string.toast_filtered_message), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun filterAuctions(query: String, categoryPos: Int, sortPos: Int) {
+        val sortBy = mapOf(0 to "ALPHABETICAL_ASC", 1 to "ALPHABETICAL_DESC", 2 to "BIDS_ASC",
+            3 to "BIDS_DESC", 4 to "RESERVE_ASC", 5 to "RESERVE_DESC", 6 to "CLOSING_LAST", 7 to "CLOSING_SOON")
+        if (query.isNullOrEmpty()) {
+            if (categoryPos == 0) {
+                getAuctions(URL(BASE_URL + "auctions?sortBy=${sortBy[sortPos]}"))
+            } else {
+                getAuctions(URL(BASE_URL + "auctions?categoryIds=${categoryPos}&sortBy=${sortBy[sortPos]}"))
+            }
+        } else if (categoryPos ==  0) {
+            getAuctions(URL(BASE_URL + "auctions?q=${query}&sortBy=${sortBy[sortPos]}"))
+        } else {
+            getAuctions(URL(BASE_URL + "auctions?q=${query}&categoryIds=${categoryPos}&sortBy=${sortBy[sortPos]}"))
+        }
+    }
 
 
 }
--- Assignments/Assignment1/app/src/main/java/com/example/metrade/SortFilterActivity.kt
+++ Assignments/Assignment1/app/src/main/java/com/example/metrade/SortFilterActivity.kt
@@ -1,17 +1,17 @@
 package com.example.metrade
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.SearchView
-import android.widget.Spinner
+import android.widget.*
 import java.util.*
 
 private lateinit var categories: Spinner
 private lateinit var searchQuery: SearchView
 private lateinit var sortBy: Spinner
+private lateinit var submitButton: Button
 
 private var selectedCategory = 0
 private var selectedSortBy = 7
@@ -24,6 +24,7 @@ class SortFilterActivity : AppCompatActivity() {
         categories = findViewById(R.id.category_spinner)
         sortBy = findViewById(R.id.sortBy_spinner)
         searchQuery = findViewById(R.id.searchQuery)
+        submitButton = findViewById(R.id.submit_button)
 
         ArrayAdapter.createFromResource(this, R.array.array_categories,
             android.R.layout.simple_dropdown_item_1line
@@ -41,20 +42,50 @@ class SortFilterActivity : AppCompatActivity() {
             sortBy.adapter = adapter
         }
 
-        val categoryListener = object : AdapterView.OnItemClickListener {
-            override fun onItemClick(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-               selectedCategory = pos
+        val categoryListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                selectedCategory = position
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                selectedCategory = 0
             }
 
         }
 
-        val sortByListener = object : AdapterView.OnItemClickListener {
-            override fun onItemClick(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                selectedSortBy = pos
+        val sortByListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                selectedSortBy = position
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                selectedSortBy = 7
             }
         }
+
+        categories.onItemSelectedListener = categoryListener
+        sortBy.onItemSelectedListener = sortByListener
 
         categories.setSelection(0)
         sortBy.setSelection(7)
+
+        submitButton.setOnClickListener {
+            val result = Intent()
+            result.putExtra("query", searchQuery.query.toString())
+            result.putExtra("category", selectedCategory)
+            result.putExtra("sort", selectedSortBy)
+            setResult(Activity.RESULT_OK, result)
+            finish()
+        }
     }
 }
